e1b50eec12e1e785322dadda48a1822213234958
mrgsfl <mar9aritams@gmail.com>
2020-10-23 23:13:47 +0800

    add solved tasks

---
+++ lesson2_1_step5.py
@@ -0,0 +1,31 @@
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+from selenium import webdriver
+import time
+
+try: 
+    link = "http://suninjuly.github.io/math.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+    option1 = browser.find_element_by_css_selector("[for='robotCheckbox']")
+    option1.click()
+    option2 = browser.find_element_by_css_selector("[for='robotsRule']")
+    option2.click()
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click() 
+    
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
---
+++ lesson2_1_step7.py
@@ -0,0 +1,32 @@
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+from selenium import webdriver
+import time
+
+try: 
+    link = "http://suninjuly.github.io/get_attribute.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element_by_id("treasure")
+    x = x_element.get_attribute("valuex")
+    y = calc(x)
+
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+    option1 = browser.find_element_by_id("robotCheckbox")
+    option1.click()
+    option2 = browser.find_element_by_id("robotsRule")
+    option2.click()
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click() 
+    
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+    
