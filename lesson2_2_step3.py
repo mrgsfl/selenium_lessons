c33cc2a40395e7d8d112c478fc0592d7f7518e3e
mrgsfl <mar9aritams@gmail.com>
2020-10-26 21:39:54 +0800

    add solved tasks

---
+++ lesson2_2_step3.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.support.ui import Select
+try: 
+    link = "http://suninjuly.github.io/selects1.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    number1 = int(browser.find_element_by_id("num1").text)
+    number2 = int(browser.find_element_by_id("num2").text)
+    summa = str(number1 + number2)
+    #print(summa)
+    select = Select(browser.find_element_by_tag_name("select"))
+    select.select_by_value(summa)
+    browser.find_element_by_class_name("btn").click()
+
+    
+    
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
